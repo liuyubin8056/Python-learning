4210844ec7590033f58ccf2cf54897ffc21a49a3
liuyubin8056 <liuyubin8056@163.com>
2025-11-13 15:28:38 +0800

    2025.11.13

---
+++ alien_invation.py
@@ -0,0 +1,37 @@
+import sys 
+import pygame
+
+from settings import Settings
+from ship import Ship
+ 
+class AlienInvasion: 
+    """管理游戏资源和行为的类""" 
+
+    def __init__(self): 
+        """初始化游戏并创建游戏资源""" 
+        pygame.init() 
+        self.settings=Settings()
+        self.screen=pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        self.clock=pygame.time.Clock()
+
+        self.ship=Ship(self)
+ 
+    def run_game(self): 
+        """开始游戏的主循环""" 
+        while True: 
+            # 侦听键盘和鼠标事件 
+            for event in pygame.event.get(): 
+                if event.type == pygame.QUIT: 
+                    sys.exit() 
+ 
+            # 让最近绘制的屏幕可见 
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            pygame.display.flip()
+            self.clock.tick(60)
+ 
+if __name__ == '__main__': 
+    # 创建游戏实例并运行游戏 
+    ai = AlienInvasion() 
+    ai.run_game()
