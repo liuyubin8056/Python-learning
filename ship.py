143099aefc9af554e8abdaaa607d594edb2a35ec
liuyubin8056 <liuyubin8056@163.com>
2025-11-20 20:51:47 +0800

    2025.11.20

--- ship.py
+++ ship.py
@@ -3,12 +3,14 @@ class Ship:
     """管理飞船的类""" 
     def __init__(self, ai_game): 
         """初始化飞船并设置其初始位置""" 
+        self.setting=ai_game.settings
         self.screen = ai_game.screen 
-        self.screen_rect = ai_game.screen.get_rect() 
+        self.screen_rect = ai_game.screen.get_rect()
         # 加载飞船图像、调整大小并获取其外接矩形 
         self.image = pygame.image.load('images/hajimi.jpg') 
         self.image = pygame.transform.scale(self.image, (50, 50))
-        self.rect = self.image.get_rect() 
+        self.rect = self.image.get_rect()
+        self.x=float(self.rect.x)
         # 每艘新飞船都放在屏幕底部的中央 
         self.rect.midbottom = self.screen_rect.midbottom
         #移动标志
@@ -17,10 +19,11 @@ class Ship:
 
     def update(self):
         #根据移动标志调整飞船位置
-        if self.moving_right:
-            self.rect.x+=1
-        if self.moving_left:
-            self.rect.x-=1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x+=self.setting.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x-=self.setting.ship_speed
+            # self.rect.x-=1  浮点型转换有争议
 
     def blitme(self): 
         """在指定位置绘制飞船""" 
